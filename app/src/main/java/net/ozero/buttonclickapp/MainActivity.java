6062f499cc7f20a95c925f2f0bf9f3199269b11f
lexa <sokolov@ozero.net>
2018-05-19 04:41:03 +0300

    Fields added

--- app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
+++ app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
@@ -2,8 +2,15 @@ package net.ozero.buttonclickapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
