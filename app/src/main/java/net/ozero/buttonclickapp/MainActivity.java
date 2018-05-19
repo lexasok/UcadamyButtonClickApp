f233413f6e5bc7aeaa52a6565a36775783deb4d0
lexa <sokolov@ozero.net>
2018-05-19 07:30:08 +0300

    OnClickListener declared, field added

--- app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
+++ app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
@@ -2,6 +2,7 @@ package net.ozero.buttonclickapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
     private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
