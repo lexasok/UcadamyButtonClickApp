622f51a3afb35b081e1b7d2805777c956b8d8899
lexa <sokolov@ozero.net>
2018-06-05 18:32:39 +0300

    Logs initialized

--- app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
+++ app/src/main/java/net/ozero/buttonclickapp/MainActivity.java
@@ -46,24 +46,32 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        Log.d(TAG, "onStart: in");
         super.onStart();
+        Log.d(TAG, "onStart: out");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
         super.onRestoreInstanceState(savedInstanceState);
         textView.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+        Log.d(TAG, "onRestoreInstanceState: out");
     }
 
     @Override
     protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
         super.onRestart();
+        Log.d(TAG, "onRestart: out");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
         super.onSaveInstanceState(outState);
         outState.putString(TEXT_VIEW_KEY, textView.getText().toString());
+        Log.d(TAG, "onSaveInstanceState: out");
     }
 
 }
